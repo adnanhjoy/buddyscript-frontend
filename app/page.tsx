03dd78747ce9a52cc2213753323f33061db01f79
Adnan Hossain <engr.adnanhossain@gmail.com>
2026-07-11 12:17:04 +0600

    feat: convert feed page from provided html file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
+import DesktopMenu from "@/components/feed/DesktopMenu";
+import LayoutMiddle from "@/components/feed/LayoutMiddle";
+import LeftSidebar from "@/components/feed/LeftSidebar";
+import MobileBottomNavigation from "@/components/feed/MobileBottomNavigation";
+import MobileMenu from "@/components/feed/MobileMenu";
+import RightSidebar from "@/components/feed/RightSidebar";
+import SwitchingButton from "@/components/feed/SwitchingButton";
+
 export default function Home() {
   return (
-    <div></div>
+    <div className="_layout _layout_main_wrapper">
+      <SwitchingButton />
+      <div className="_main_layout">
+        <DesktopMenu />
+        <MobileMenu />
+        <MobileBottomNavigation />
+        {/* <!-- Mobile Bottom Navigation End --> */}
+        {/* <!-- Main Layout Structure --> */}
+        <div className="container _custom_container">
+          <div className="_layout_inner_wrap">
+            <div className="row">
+              {/* <!-- Left Sidebar --> */}
+              <LeftSidebar />
+              {/* <!-- Left Sidebar --> */}
+              {/* <!-- Layout Middle --> */}
+              <LayoutMiddle />
+              {/* <!-- Layout Middle --> */}
+              {/* <!-- Right Sidebar --> */}
+              <RightSidebar />
+              {/* <!-- Right Sidebar --> */}
+            </div>
+          </div>
+        </div>
+        {/* <!-- Main Layout Structure --> */}
+      </div>
+    </div>
   );
 }
